ff6fcb3285ffd362391ad16f183677e06dbd332c
Cody Reed <cody2j2reed@gmail.com>
2018-12-15 15:48:57 -0500

    Update app.js
    Added comments for planning and preparing app design and functionality

---
+++ app.js
@@ -0,0 +1,10 @@
+// Problem: Need a simple way to look at a Treehouse user's badge and JavaScript point(s) count from a web browser
+// Solution: Use Node.js to perform the profile look ups and server our template via HTTP
+
+// 1. Create a web server
+
+// 2. Handle HTTP route Get / and POST/ i.e Home
+
+// 3. Handle HTTP route GET /:username i.e / codyreed3
+
+// 4. Function that handles the reading of files and merge in value
